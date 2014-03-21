1ab1a6493d7d350ba1244ae80c1c42e4b3e45cb3
Argha <argha.sarkar1994@gmail.com>
2014-03-21 14:30:18 +0000

    test commit

---
+++ RoboCodeBotE/src/MyBotEclipse/RoboCodeBotE.java
@@ -0,0 +1,20 @@
+package MyBotEclipse;
+
+import robocode.*;
+
+public class RoboCodeBotE extends Robot{
+
+	public void run() {
+        while (true) {
+            ahead(50);
+            turnGunRight(180);
+            back(75);
+            turnGunRight(80);
+        }
+    }
+    
+    public void onScannedRobot(ScannedRobotEvent e) {
+        fire(1);
+    }
+
+}
